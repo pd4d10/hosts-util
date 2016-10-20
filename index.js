6c11748746119030f32f32355b95d674fc46998b
pd4d10 <pd4d10@gmail.com>
2016-10-20 16:51:49 +0800

    add `stringify`, and more tests

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function parse(hosts) {
+function isUndefined(val) {
+  return typeof val === 'undefined'
+}
+
+function isString(val) {
+  return typeof val === 'string'
+}
+
+function isObject(val) {
+  return val !== null && typeof val === 'object'
+}
+
+function parse(hosts, opt) {
+  if (!isString(hosts)) {
+    throw new Error('Parse\'s first param should be a string')
+  }
+
+  if (!isObject(opt)) {
+    opt = {}
+  }
+
   return hosts.split('\n')
-    .map(function (x) {
-      return x.split(/\s+/)
+    .filter(function (str) { // remove blank line
+      return /\s*/.test(str)
+    })
+    .map(function (line) {
+      return line.trim().split(/\s+/)
     })
     .reduce(function (obj, line) {
       var ip = line[0]
@@ -14,10 +37,22 @@ function parse(hosts) {
     }, {})
 }
 
-function stringify(json) {
+function stringify(json, opt) {
+  if (!isObject(json)) {
+    throw new Error('Stringify\'s first param should be an object')
+  }
+
+  if (!isObject(opt)) {
+    opt = {}
+  }
+
+  if (isUndefined(opt.seperate)) {
+    opt.seperate = ' '
+  }
+
   return Object.keys(json)
     .map(function (key) {
-      return json[key] + ' ' +  key
+      return json[key] + opt.seperate + key
     })
     .join('\n')
 }
--- test.js
+++ test.js
@@ -5,7 +5,16 @@ var parse = hostsUtil.parse
 var stringify = hostsUtil.stringify
 
 describe('parse', function () {
-  it('should', function () {
+  it('should throw error when first argument is not a string', function () {
+    assert.throws(
+      function () {
+        parse([])
+      },
+      /Parse's first param should be a string/
+    )
+  })
+
+  it('should handle single line', function () {
     assert.deepEqual(
       parse('127.0.0.1 www.example.com'),
       {
@@ -23,7 +32,7 @@ describe('parse', function () {
     )
   })
 
-  it('should handle multi hosts', function () {
+  it('should handle multi hosts in one line', function () {
     assert.deepEqual(
       parse('127.0.0.1 www.example.com www.example.org'),
       {
@@ -42,8 +51,44 @@ describe('parse', function () {
       }
     )
   })
+
+  it('should handle blank line', function () {
+    assert.deepEqual(
+      parse('127.0.0.1 www.example.com\n\n192.168.0.1 www.example.org'),
+      {
+        'www.example.com': '127.0.0.1',
+        'www.example.org': '192.168.0.1'
+      }
+    )
+  })
 })
 
 describe('stringify', function () {
+  it('should throw error when first argument is not an object', function () {
+    assert.throws(
+      function () {
+        stringify('I am a string')
+      },
+      /Stringify's first param should be an object/
+    )
+  })
 
+  it('should', function () {
+    assert.equal(
+      stringify({
+        'www.example.com': '127.0.0.1'
+      }),
+      '127.0.0.1 www.example.com'
+    )
+  })
+
+  it('should', function () {
+    assert.equal(
+      stringify({
+        'www.example.com': '127.0.0.1',
+        'www.example.org': '192.168.0.1',
+      }),
+      '127.0.0.1 www.example.com\n192.168.0.1 www.example.org'
+    )
+  })
 })
